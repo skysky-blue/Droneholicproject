aa06ac2e93a937ad01af8d6b5d7aba06df4f0869
skysky-blue <jeongchanung2005@gmail.com>
2022-07-26 21:22:59 +0900

    1.slider 버그 고침 2.release 시 초기화 3.yaw값 추가

--- controll.py
+++ controll.py
@@ -1,17 +1,20 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QSlider, QDial, QPushButton
 from PyQt5.QtCore import Qt
-
+import time
 
 class MyApp(QWidget):
 
 
     def __init__(self):
-        self.front = 0 #앞뒤 움직임 값
-        self.side = 0  #좌우 움직임 값
-        self.up = 0    #상하 움직임 값
+
+        self.front = 0 #pitch 움직임 값
+        self.side = 0  #roll 움직임 값
+        self.up = 0    #throttle 움직임 값
+        self.yaw = 0   #yaw 움직임 값
         super().__init__()
         self.initUI()
+
 
     def initUI(self):
         self.slider_front = QSlider(Qt.Vertical, self) #front 슬라이더 설정
@@ -24,8 +27,13 @@ class MyApp(QWidget):
         self.slider_side.setRange(-1, 1)
         self.slider_side.setSingleStep(1)
 
+        self.slider_yaw = QSlider(Qt.Vertical, self) #yaw 슬라이더 설정
+        self.slider_yaw.move(130, 30)
+        self.slider_yaw.setRange(-1,1)
+        self.slider_yaw.setSingleStep(1)
+
         self.slider_up = QSlider(Qt.Vertical, self) #up 슬라이더 설정
-        self.slider_up.move(130, 30)
+        self.slider_up.move(180, 30)
         self.slider_up.setRange(0, 100)
         self.slider_up.setSingleStep(1)
 
@@ -38,38 +46,73 @@ class MyApp(QWidget):
         self.setGeometry(300, 300, 400, 200)
         self.show()
 
-        self.keyReleaseEvent(Qt.key_W)
-        while True:
-            print(self.keyReleaseEvent(self.key_W))
+
+        # while True:
+            # print(self.keyReleaseEvent(self.key_W))
 
     def button_clicked(self):
         self.slider_front.setValue(0)
         self.slider_side.setValue(0)
         self.slider_up.setValue(0)
+        self.slider_yaw.setValue(0)
 
     # def value_(self):
 
-    # keyRealease
+
+    def keyPressEvent(self, eventQKeyEvent):
+        key = eventQKeyEvent.key()
+        if key == 54:
+            print('pressed')  # 6
+
+    def keyReleaseEvent(self, eventQKeyEvent):
+        key = eventQKeyEvent.key()
+        if key == 87 and not eventQKeyEvent.isAutoRepeat():
+            self.front = 0
+            self.slider_front.setValue(self.front)
+        elif key == 83 and not eventQKeyEvent.isAutoRepeat():
+            self.front = 0
+            self.slider_front.setValue(self.front)
+        elif key == 65 and not eventQKeyEvent.isAutoRepeat():
+            self.side = 0
+            self.slider_side.setValue(self.side)
+        elif key == 68 and not eventQKeyEvent.isAutoRepeat():
+            self.side = 0
+            self.slider_side.setValue(self.side)
+        elif key == 81 and not eventQKeyEvent.isAutoRepeat():
+            self.yaw = 0
+            self.slider_yaw.setValue(self.yaw)
+        elif key == 69 and not eventQKeyEvent.isAutoRepeat():
+            self.yaw = 0
+            self.slider_yaw.setValue(self.yaw)
+
+
+
     def keyPressEvent(self, e):
 
-        if e.key() == Qt.Key_W: #앞뒤 조정
+        if e.key() == Qt.Key_W: #pitch 조정
             self.front += 1
             self.slider_front.setValue(self.front)
         elif e.key() == Qt.Key_S:
             self.front -= 1
             self.slider_front.setValue(self.front)
-        elif e.key() == Qt.Key_A: #좌우 조정
+        elif e.key() == Qt.Key_A: #roll 조정
             self.side += 1
             self.slider_side.setValue(self.side)
         elif e.key() == Qt.Key_D:
             self.side -= 1
             self.slider_side.setValue(self.side)
-        elif e.key() == Qt.Key_Q: #상하 조정
+        elif e.key() == Qt.Key_R: #상하 조정
             self.up += 5
             self.slider_up.setValue(self.up)
-        elif e.key() == Qt.Key_E:
+        elif e.key() == Qt.Key_F:
             self.up -= 5
             self.slider_up.setValue(self.up)
+        elif e.key() == Qt.Key_Q: #yaw 조정
+            self.yaw += 1
+            self.slider_yaw.setValue(self.yaw)
+        elif e.key() == Qt.Key_E:
+            self.yaw -= 1
+            self.slider_yaw.setValue(self.yaw)
 
 
 if __name__ == '__main__':
